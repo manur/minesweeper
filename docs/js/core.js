04a5acc524c42836140b60a96c0a9e90cb0144b0
Manu Raghavan <manu.raghavan@gmail.com>
2019-11-27 14:31:51 +0530

    Web app

---
+++ docs/js/core.js
@@ -0,0 +1,266 @@
+var Board = function(cols, rows, numMines) {
+  var board = this;
+
+  /* Dimensions */
+  if(!cols) {
+    cols = 16;
+  }
+  if(!rows) {
+    rows = 16;
+  }
+  
+  var numTiles = cols * rows;
+
+  this.cols = cols;
+  this.rows = rows;
+  this.numTiles = numTiles;
+  this.numMinesMarked = 0;
+
+  if(!numMines) {
+    numMines = Math.floor(Math.sqrt(Math.floor(Math.sqrt(numTiles)))) * 20;
+  }
+  this.numMines = numMines;
+
+  var randomSample = function(arr, size) {
+    var shuffled = arr.slice(0), i = arr.length, temp, index;
+    while (i--) {
+      index = Math.floor((i + 1) * Math.random());
+      temp = shuffled[index];
+      shuffled[index] = shuffled[i];
+      shuffled[i] = temp;
+    }
+    return shuffled.slice(0, size);
+  };
+
+  /* Populate mines */
+  var linear = Array(numTiles);
+  var idx;
+  for(idx = 0; idx < numTiles; idx += 1) {
+    linear[idx] = idx;
+  }
+  var mineIdx = randomSample(linear, numMines);
+  this.mineIdx = mineIdx;
+  this.createPaddedMatrix();
+  this.createPressedMatrix();
+  this.markTiles();
+};
+
+
+Board.prototype.getColIndex = function(idx) {
+  return idx % this.cols;
+};
+
+Board.prototype.getRowIndex = function(idx) {
+  return Math.floor(idx / this.cols);
+};
+
+
+Board.prototype.neighbors = function(paddedCol, paddedRow) {
+  var board = this;
+  return [
+    board._paddedMatrix[paddedCol - 1][ paddedRow - 1],
+    board._paddedMatrix[paddedCol - 1][ paddedRow    ],
+    board._paddedMatrix[paddedCol - 1][ paddedRow + 1],
+    board._paddedMatrix[paddedCol    ][ paddedRow - 1],
+    board._paddedMatrix[paddedCol    ][ paddedRow + 1],
+    board._paddedMatrix[paddedCol + 1][ paddedRow - 1],
+    board._paddedMatrix[paddedCol + 1][ paddedRow    ],
+    board._paddedMatrix[paddedCol + 1][ paddedRow + 1],
+    ];
+};
+
+
+Board.prototype.markTiles = function() {
+  var board = this;
+  var col, row, paddedCol, paddedRow;
+
+  var rcs = _.map(this.mineIdx, function(idx) {
+    var colIndex = board.getColIndex(idx);
+    var rowIndex = board.getRowIndex(idx);
+
+    return [colIndex, rowIndex];
+  });
+
+  console.log(rcs);
+
+  for(col = 0; col < board.cols; col +=1) {
+    for(row = 0; row < board.rows; row +=1) {
+      paddedCol = col + 1;
+      paddedRow = row + 1;
+
+      if(_.any(rcs, function(idx) {
+        return col === idx[0] &&  row === idx[1];
+      })) {
+        // Mark tile as mine
+        board._paddedMatrix[paddedCol][paddedRow] = null;
+      }
+    }
+  }
+
+  for(col = 0; col < board.cols; col +=1) {
+    for(row = 0; row < board.rows; row +=1) {
+      paddedCol = col + 1;
+      paddedRow = row + 1;
+
+      if(!_.isNull(board._paddedMatrix[paddedCol][paddedRow])) {
+        var mark = 0;
+        var neighbors = board.neighbors(paddedCol, paddedRow);
+        mark += _.select(neighbors, function(neighbor) {
+          return _.isNull(neighbor);  
+        }).length;
+        board._paddedMatrix[paddedCol][paddedRow] = mark;
+      }
+    }
+  }
+};
+
+Board.prototype.createPaddedMatrix = function() {
+  this._paddedMatrix = Array(this.cols + 2);
+
+  for(var paddedCol = 0; paddedCol < this.cols + 2 ; paddedCol += 1) {
+    this._paddedMatrix[paddedCol] = Array(this.rows + 2);
+    _.each(this._paddedMatrix[paddedCol], function(elem) {
+      elem = 0;
+    });
+  }
+
+  return this._paddedMatrix;
+};
+
+Board.prototype.createPressedMatrix = function() {
+  this._pressedMatrix = Array(this.cols + 2);
+
+  for(var pressedCol = 0; pressedCol < this.cols + 2 ; pressedCol += 1) {
+    this._pressedMatrix[pressedCol] = Array(this.rows + 2);
+    _.each(this._pressedMatrix[pressedCol], function(elem) {
+      elem = false;
+    });
+  }
+
+  return this._pressedMatrix;
+};
+
+Board.prototype.getPaddedMatrix = function() {
+  return this._paddedMatrix;
+};
+
+Board.prototype.press = function(paddedCol, paddedRow, done) {
+  var board = this;
+  var opens;
+
+  if(board._pressedMatrix[paddedCol][paddedRow] === 'mine') {
+    return; // no-op
+  }
+
+  board._pressedMatrix[paddedCol][paddedRow] = true;
+
+  if(_.isNull(board._paddedMatrix[paddedCol][paddedRow])) {
+
+    done({
+      terminate: {
+              paddedCol: paddedCol,
+              paddedRow: paddedRow,
+              termination: 'mine'
+            }
+    });
+
+  } else {
+
+    done({
+      open: {
+          paddedCol: paddedCol,
+          paddedRow: paddedRow
+      }
+    });
+
+    board.openAnyUnopenedNeighbors(paddedCol, paddedRow, done);
+
+  }
+};
+
+Board.prototype.allMinesDetected = function() {
+  var board = this;
+
+  var numMarkedMines = 0;
+  var allDetected = true;
+
+  for(var c=0; c<board.cols; c+=1) {
+    for(var r=0; r<board.rows; r+=1) {
+
+      if(board._pressedMatrix[c+1][r+1] === 'mine' && board._paddedMatrix[c+1][r+1] !== null) {
+        allDetected = false;
+      }
+
+      if(board._pressedMatrix[c+1][r+1] !== 'mine' && board._paddedMatrix[c+1][r+1] === null) {
+        allDetected = false;
+      }
+
+    }
+  }
+
+  return allDetected;
+};
+
+Board.prototype.markAsMine = function(paddedCol, paddedRow, done) {
+  var board = this;
+
+  if(board._pressedMatrix[paddedCol][paddedRow] !== 'mine') {
+    board._pressedMatrix[paddedCol][paddedRow] = 'mine'; // denotes mark as mine
+    board.numMinesMarked += 1;
+  } else {
+    board._pressedMatrix[paddedCol][paddedRow] = false; // revert mark
+    board.numMinesMarked -= 1;
+  }
+
+  if(board.allMinesDetected()) {
+    done({
+      terminate: {
+                    paddedCol: paddedCol,
+                    paddedRow: paddedRow,
+                    termination: 'complete'
+                  }
+    });
+  }
+}
+
+Board.prototype.openAnyUnopenedNeighbors = function(paddedCol, paddedRow, done) {
+  var board = this;
+
+  const neighboringIndices = [
+   [paddedCol - 1, paddedRow - 1],
+   [paddedCol - 1, paddedRow    ],
+   [paddedCol - 1, paddedRow + 1],
+   [paddedCol    , paddedRow - 1],
+   [paddedCol    , paddedRow + 1],
+   [paddedCol + 1, paddedRow - 1],
+   [paddedCol + 1, paddedRow    ],
+   [paddedCol + 1, paddedRow + 1],
+  ];
+
+  _.each(neighboringIndices, function(colRow) {
+    const _paddedRow = colRow[1];
+    const _paddedCol = colRow[0];
+
+    const neighborMark = board._paddedMatrix[_paddedCol][_paddedRow];
+    //console.log("neighbor mark: ", neighborMark);
+
+    if(!board._pressedMatrix[_paddedCol][_paddedRow] && !_.isNull(board._paddedMatrix[_paddedCol][_paddedRow])) {
+      board._pressedMatrix[_paddedCol][_paddedRow] = true;
+
+      done({
+        open: {
+                paddedCol: _paddedCol,
+                paddedRow: _paddedRow
+              }
+      });
+
+      if(neighborMark === 0) {
+        setTimeout(function() {
+          board.openAnyUnopenedNeighbors(_paddedCol, _paddedRow, done);
+        }, 0);
+      }
+    }
+  });
+};
+
+
---
+++ docs/js/reverse-minesweeper.js
@@ -0,0 +1,249 @@
+
+const Template = function(templateSelector) {
+  return Handlebars.compile($(templateSelector).html());
+};
+
+var Model = Backbone.Model.extend({
+
+
+  initialize: function() {
+
+                function getParameterByName(name, url) {
+                  if (!url) url = window.location.href;
+                  name = name.replace(/[\[\]]/g, "\\$&");
+                  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+                  results = regex.exec(url);
+                  if (!results) return null;
+                  if (!results[2]) return '';
+                  return decodeURIComponent(results[2].replace(/\+/g, " "));
+                }
+
+                var rows = $('input.rows').html() || 16;
+                var cols = $('input.cols').html() || 16;
+                var numMines = $('input.mines').html() || 40;
+
+                this.board = new Board(rows, cols, numMines);
+                this.paddedMatrix = this.board.getPaddedMatrix();
+              },
+
+    press: function(paddedCol, paddedRow) {
+             var model = this;
+             this.board.press(paddedCol, paddedRow, function(update) {
+
+               // console.log('update', update);
+
+               if(update.open) {
+                 model.trigger('open', {
+                   paddedCol: update.open.paddedCol,
+                   paddedRow: update.open.paddedRow
+                 });
+               }
+
+               if(update.terminate) {
+                 model.trigger('terminate', {
+                   paddedCol: update.terminate.paddedCol,
+                   paddedRow: update.terminate.paddedRow,
+                   termination: update.terminate.termination
+                 });
+               }
+             });
+           },
+
+    markAsMine: function(paddedCol, paddedRow) {
+                  var model = this;
+                  this.board.markAsMine(paddedCol, paddedRow, function(update) {
+                    // console.log('markAsMine update', update);
+
+                    if(update.terminate) {
+                      model.trigger('terminate', {
+                        paddedCol: update.terminate.paddedCol,
+                        paddedRow: update.terminate.paddedRow,
+                        termination: update.terminate.termination
+                      });
+                    }
+
+                  });
+                }
+});
+
+var Minesweeper = Backbone.View.extend({
+  template: Template('.minesweeper.template'),
+  cellTemplate: Template('.minesweeper-cell.template'),
+
+  initialize: function() {
+    this.model = new Model();
+    this.listenTo(this.model, 'open', this.open);
+    this.listenTo(this.model, 'terminate', this.terminate);
+  },
+
+  events: {
+            "click          table.active.board td button": "markAsMine",
+            "contextmenu    table.active.board td button": "markAsMine"
+          },
+
+  render: function() {
+            this.$el.html(this.template({
+            }));
+
+            this.$board = this.$el.find('table.board');
+
+            for(var paddedCol = 1; paddedCol < this.model.paddedMatrix.length - 1; paddedCol += 1) {
+              var $col = $('<tr></tr>');
+              for(var paddedRow = 1; paddedRow < this.model.paddedMatrix[paddedCol].length - 1; paddedRow += 1) {
+                const elem = this.model.paddedMatrix[paddedCol][paddedRow];
+                const str = (elem === null) ? '' : (elem === 0) ? '' : elem;
+
+                const $cell = $(this.cellTemplate({
+                  text: str,
+                  paddedCol: paddedCol,
+                  paddedRow: paddedRow,
+                  isMine: elem === null,
+                  mark: elem
+                }));
+
+                $col.append($cell);
+              }
+              this.$board.append($col);
+            }
+          },
+
+  open: function(openevt) {
+          // console.log("openevt", openevt);
+          var $cell = this.$el.find('[data-padded-column="' + openevt.paddedCol + '"][data-padded-row="' + openevt.paddedRow + '"]');
+          $cell.addClass('pressed');
+        },
+
+  terminate: function(terminateEvt) {
+               var $cell = this.$el.find('[data-padded-column="' + terminateEvt.paddedCol + '"][data-padded-row="' + terminateEvt.paddedRow + '"]');
+
+               $cell.closest('table.board.active').removeClass('active');
+
+               if(terminateEvt.termination === 'complete') {
+                 $('.completed .victory-message').html(this.victoryMessage());
+                 $('.completed').show();
+                 $('.restart-game').show();
+               } else {
+                 // hit a mine
+                 $('.lost .defeat-message').html(this.defeatMessage());
+                 $('.lost').show();
+                 $('.restart-game').show();
+               }
+
+               this.stopTimer();
+
+               console.log(terminateEvt.termination);
+             },
+
+  victoryMessage: function() {
+                    var items = [
+                      'Victory is your birthright. ',
+                      'Awww, yeah! Nice work. ',
+                      'That\'s some sweet clicking. ',
+                      'Mine marking. Like A Boss. ',
+                      'Winning! '
+                    ];
+
+                    return items[Math.floor(Math.random()*items.length)];
+                  },
+
+
+  defeatMessage: function() {
+                    var items = [
+                      'Oh, dear. Not your day, is it? ',
+                      'Ah! Losing, your old nemesis. ',
+                      ':( Better luck next time. ',
+                      'There\'s always tomorrow... '
+                    ];
+
+                    return items[Math.floor(Math.random()*items.length)];
+                  },
+
+  markAsMine: function(evt) {
+                      this.startTimer();
+
+                      var $target = $(evt.currentTarget);
+                      var $td = $target.closest('td');
+                      
+                      $td.toggleClass('mark-as-mine');
+                      var $cell = $target.closest('td')
+
+                      var paddedCol = $cell.data('paddedColumn');
+                      var paddedRow = $cell.data('paddedRow');
+                      this.model.markAsMine(paddedCol, paddedRow);
+
+                      this.mineCount();
+                      
+                      evt.stopPropagation();
+                      evt.preventDefault();
+                    },
+
+  mineCount: function() {
+               var $mineCount  = $('.mine-count');
+               var count = this.model.board.numMines - this.model.board.numMinesMarked;
+               if(count > -1 && count < 10) {
+                 count = '0' + count;
+               } else if(count <- -10) {
+                 count = '--';
+               }
+               $mineCount.html(count);
+             },
+
+  startTimer: function() {
+                if(this.timer) {
+                  return;
+                }
+
+                var view = this;
+                var $time = $('.time-elapsed');
+                var setClock = function(time) {
+                  var seconds = time % 60 + '';
+                  if(seconds.length === 1) {
+                    seconds = '0' + seconds;
+                  }
+                  $time.html(Math.floor(time / 60) + ':' + seconds);
+                };
+
+                var start = Date.now();
+                this.timer = setInterval(function() {
+                  setClock(Math.round((Date.now() - start) / 1000));
+                }, 1000);
+
+              },
+
+  stopTimer: function() {
+               clearInterval(this.timer);
+             }
+});
+
+
+var App = Backbone.View.extend({
+  template: Template('.template.app'),
+
+    initialize: function() {
+      this.minesweeper = new Minesweeper({
+      });
+    },
+
+  events: {
+            "click          .reload":   "reload"
+          },
+
+  reload: function() {
+            window.location.reload();
+          },
+
+  render: function() {
+            this.$el.html(this.template());
+            this.minesweeper.render();
+            this.$el.find('.minesweeper-container').html(this.minesweeper.$el);
+          }
+});
+
+window.app = new App({
+  el: $('#app-container')
+});
+
+app.render();
+
+
+
